fac2c2b5a35e17a946e36af16d2210582b7519ca
Syed Noman Ali <66224759+nomansd1@users.noreply.github.com>
2021-04-14 23:58:59 +0500

    new screen added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // components
 import Navbar from './Components/Navbar'
 import Home from './Components/Screens/Home screen/Home'
 import Footer from './Components/Footer'
+import Experts from './Components/Screens/Experts Screen/Experts'
+
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Home/>
-      <Footer/>
+      <BrowserRouter>
+        <Navbar/>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/experts" exact component={Experts} />
+        </Switch>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
