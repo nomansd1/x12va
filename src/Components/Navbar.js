107f7968f2b25d417b4a8ce6d43d2285b059583d
Syed Noman Ali <66224759+nomansd1@users.noreply.github.com>
2021-05-03 20:08:17 +0500

    error resolved

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -42,7 +42,7 @@ function Navbar({ cName }) {
                 <NavLink to="/experts" className="nav__link"><span>Experts</span></NavLink>
                 <NavLink to="/" className="nav__link"><span>Work for us</span></NavLink>
                 <NavLink to="/verifiers" className="nav__link"><span>Verifiers</span></NavLink>
-                <NavLink to="/" className="nav__link"><span>Support us</span></NavLink>
+                <NavLink to="/profile" className="nav__link"><span>Support us</span></NavLink>
                 <span className="nav__link"><img src={signinIcon} className="signin__icon" />Sign in</span>
                 <button>Create Account</button>
                 <div className="close__menu" onClick={()=> setOpen(false)}><CloseIcon/></div>
