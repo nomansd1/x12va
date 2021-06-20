2cc6cee70fd4061423aee466c0ee2b8af5fd9592
Syed Noman Ali <66224759+nomansd1@users.noreply.github.com>
2021-06-20 18:54:19 +0500

    several screens added including create account(SignUp)

--- src/App.js
+++ src/App.js
@@ -18,6 +18,15 @@ import Aboutus from './Components/Screens/About Screen/Aboutus';
 import Help from './Components/Screens/Help Screen/Help';
 import TrackOrder from './Components/Screens/Track order screen/TrackOrder';
 import Work from './Components/Screens/Work for us screen/Work';
+import VerifyOrders from './Components/Screens/Verifiers Orders Screen/VerifyOrders';
+import TrackOrderFiles from './Components/Screens/Track order screen/TrackOrderFiles';
+import UpdateOrder from './Components/Screens/Track order screen/UpdateOrder';
+import UpdateOrderFile from './Components/Screens/Track order screen/UpdateOrderFile';
+import ExpertResult from './Components/Screens/Experts Results Screen/ExpertResult';
+import ExpertResultFound from './Components/Screens/Experts Results Screen/ExpertResultFound';
+import CreditCardDetails from './Components/Screens/Credit Card Details Screen/CreditCardDetails';
+import UserReferenceCode from './Components/Screens/User Reference Code Screen/UserReferenceCode';
+import Signup from './Components/Screens/SignupScreen/Signup';
 
 
 function App() {
@@ -39,6 +48,12 @@ function App() {
               cName="navbar__color"
             />
             <Verifiers/>
+          </Route>
+          <Route path="/verifyorders" exact>
+            <Navbar
+              cName="navbar__color"
+            />
+            <VerifyOrders/>
           </Route>
           <Route path="/profile" exact>
             <Navbar
@@ -70,11 +85,59 @@ function App() {
             />
             <TrackOrder/>
           </Route>
+          <Route path="/trackorderfiles" exact>
+            <Navbar
+              cName="navbar__color"
+            />
+            <TrackOrderFiles/>
+          </Route>
+          <Route path="/updateorder" exact>
+            <Navbar
+              cName="navbar__color"
+            />
+            <UpdateOrder/>
+          </Route>
+          <Route path="/updateorderfile" exact>
+            <Navbar
+              cName="navbar__color"
+            />
+            <UpdateOrderFile/>
+          </Route>
           <Route path="/workforus" exact>
             <Navbar
               cName="navbar__color"
             />
             <Work/>
+          </Route>
+          <Route path="/expertresults" exact>
+            <Navbar
+              cName="navbar__color"
+            />
+            <ExpertResult/>
+          </Route>
+          <Route path="/expertresultsfound" exact>
+            <Navbar
+              cName="navbar__color"
+            />
+            <ExpertResultFound/>
+          </Route>
+          <Route path="/carddetails" exact>
+            <Navbar
+              cName="navbar__color"
+            />
+            <CreditCardDetails/>
+          </Route>
+          <Route path="/referencecode" exact>
+            <Navbar
+              cName="navbar__color"
+            />
+            <UserReferenceCode/>
+          </Route>
+          <Route path="/signup" exact>
+            <Navbar
+              cName="navbar__color"
+            />
+            <Signup/>
           </Route>
         </Switch>
         <Footer/>
